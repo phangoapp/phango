fc0f055586c3d465914bac6581eeb7eed4003fbd
Antonio de la Rosa <webmaster@web-t-sys.com>
2015-10-14 19:59:04 +0200

    Addded docs.sh

---
+++ docs.sh
@@ -0,0 +1,2 @@
+vendor/bin/apigen generate --source vendor/phangoapp/phalibs --source vendor/phangoapp/phamodels/ --source vendor/phangoapp/pharouter/ --source vendor/phangoapp/phaview/ --source vendor/phangoapp/phautils/ --destination docs
+
