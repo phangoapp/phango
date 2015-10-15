1b7a162b093b5a3b0da3522b3feb7b9ed31745f8
Antonio de la Rosa <webmaster@web-t-sys.com>
2015-10-15 03:14:38 +0200

    Added new i18n files

---
+++ i18n/en-US/error_model.php
@@ -0,0 +1,17 @@
+<?php
+
+use PhangoApp\PhaI18n\I18n;
+
+I18n::$lang['error_model']['cant_insert']='Can\\';
+
+I18n::$lang['error_model']['any_field_found']='Not valid fields found';
+
+I18n::$lang['error_model']['cant_update']='Can\\';
+
+I18n::$lang['error_model']['check_error_field']='Error in field';
+
+I18n::$lang['error_model']['check_error_field_required']='Error: Field required';
+
+I18n::$lang['error_model']['check_error_enctype_for_upload_file']='Please, check enctype form of file form';
+
+?>
--- i18n/en-US/users.php
+++ i18n/en-US/users.php
@@ -18,4 +18,44 @@ I18n::$lang['users']['register']='Register in the web';
 
 I18n::$lang['users']['remember_password_explain']='Please enter the email address you registered on the website, which is where you receive your new password';
 
+I18n::$lang['users']['user_error_nick_or_pass']='Wrong user or password';
+
+I18n::$lang['users']['lost_name']='You requested a new password';
+
+I18n::$lang['users']['hello_lost_pass']='Hello, you have requested a new password.';
+
+I18n::$lang['users']['explain_code_pass']='You have requested a new password. Copy and paste the following url into your browser, and a new password will be generated for you. If you did not request this operation, ignore this message.';
+
+I18n::$lang['users']['copy_paste_code']='Copy and paste the following url';
+
+I18n::$lang['users']['explain_email_code_pass']='You have requested a new password. Copy and paste the following url into your browser, and a new password will be generated for you. If you did not request this operation, ignore this message.';
+
+I18n::$lang['users']['cannot_email_code_pass']='We can not send to your email the instructions to change your password. Please contact the administrator of this site to solve the problem.';
+
+I18n::$lang['users']['error_db_pass']='Error, mail format is wrong';
+
+I18n::$lang['users']['success_change_password']='The password was changed successfully.';
+
+I18n::$lang['users']['hello_lost_pass_successful']='Hello, we have changed your password and is shown below. With these data should be back online in the system.';
+
+I18n::$lang['users']['user_data']='User\\';
+
+I18n::$lang['users']['user']='User';
+
+I18n::$lang['users']['new_pass']='New password';
+
+I18n::$lang['users']['success_change_password_explain']='We have sended to your email, the new password.';
+
+I18n::$lang['users']['error_sending_mail_change_password']='We can not send your new password to your email at this time, we are showing your user data in plain text. For added security, change your password once achieved identified in the system again.';
+
+I18n::$lang['users']['error_token_pass']='Error: incorrect code used to change a password.';
+
+I18n::$lang['users']['user_or_email_exists']='User or email exists';
+
+I18n::$lang['users']['accept_cond_register']='Accept registration conditions';
+
+I18n::$lang['users']['pasword_not_equal_repeat_password']='Passwords are not equal';
+
+I18n::$lang['users']['cannot_insert_user_email_or_user']='A user already exists with this email or username';
+
 ?>
