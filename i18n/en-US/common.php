3563b39ef0e902411e9980d8bba1cb9cf9e84986
Antonio de la Rosa <webmaster@web-t-sys.com>
2015-10-15 16:54:37 +0200

    Fixes in language

--- i18n/en-US/common.php
+++ i18n/en-US/common.php
@@ -2,24 +2,6 @@
 
 use PhangoApp\PhaI18n\I18n;
 
-I18n::$lang['common']['login']='Login';
-
-I18n::$lang['common']['send']='Send';
-
-I18n::$lang['common']['with_*_field_required']='* Field required';
-
-I18n::$lang['common']['order_by']='Order by';
-
-I18n::$lang['common']['in_order']='By order';
-
-I18n::$lang['common']['search']='Search';
-
-I18n::$lang['common']['search_by']='Search by';
-
-I18n::$lang['common']['reset']='Reset';
-
-I18n::$lang['common']['order_and_search']='Order and search';
-
 I18n::$lang['common']['home']='Home';
 
 I18n::$lang['common']['add_new_item']='Add new item';
@@ -106,6 +88,24 @@ I18n::$lang['common']['asc']='Ascendent';
 
 I18n::$lang['common']['desc']='Descendent';
 
+I18n::$lang['common']['search']='Search';
+
+I18n::$lang['common']['reset']='Reset';
+
 I18n::$lang['common']['submit']='Submit';
+
+I18n::$lang['common']['login']='Login';
+
+I18n::$lang['common']['send']='Send';
+
+I18n::$lang['common']['with_*_field_required']='* Field required';
+
+I18n::$lang['common']['order_by']='Order by';
+
+I18n::$lang['common']['in_order']='By order';
+
+I18n::$lang['common']['search_by']='Search by';
+
+I18n::$lang['common']['order_and_search']='Order and search';
 
 ?>
--- i18n/es-ES/common.php
+++ i18n/es-ES/common.php
@@ -2,27 +2,9 @@
 
 use PhangoApp\PhaI18n\I18n;
 
-I18n::$lang['common']['login']='Login';
-
-I18n::$lang['common']['send']='Send';
-
-I18n::$lang['common']['with_*_field_required']='* Field required';
-
-I18n::$lang['common']['order_by']='Order by';
-
-I18n::$lang['common']['in_order']='By order';
-
-I18n::$lang['common']['search']='Search';
-
-I18n::$lang['common']['search_by']='Search by';
-
-I18n::$lang['common']['reset']='Reset';
-
-I18n::$lang['common']['order_and_search']='Order and search';
-
 I18n::$lang['common']['home']='Home';
 
-I18n::$lang['common']['add_new_item']='Add new item';
+I18n::$lang['common']['add_new_item']='Añadir nuevo elemento';
 
 I18n::$lang['common']['add_new_item_success']='Added new item succesfully';
 
@@ -105,6 +87,10 @@ I18n::$lang['common']['press_here_redirecting']='Press here for redirecting';
 I18n::$lang['common']['asc']='Ascendent';
 
 I18n::$lang['common']['desc']='Descendent';
+
+I18n::$lang['common']['search']='Search';
+
+I18n::$lang['common']['reset']='Reset';
 
 I18n::$lang['common']['submit']='Submit';
 
