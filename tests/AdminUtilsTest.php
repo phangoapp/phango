547b6bc2f0bf1ccf71fb4cf5688fc85c847b1d43
Antonio de la Rosa <webmaster@web-t-sys.com>
2015-12-22 23:09:34 +0100

    Simple fix in test

--- tests/AdminUtilsTest.php
+++ tests/AdminUtilsTest.php
@@ -1,17 +1,26 @@
 <?php
 
 use PhangoApp\PhaLibs\AdminUtils;
+use PhangoApp\PhaRouter\Routes;
 
 include("../vendor/autoload.php");
+/*
+if(!defined('ADMIN_FOLDER'))
+{
 
-define('ADMIN_FOLDER', 'admin');
+    define('ADMIN_FOLDER', 'admin');
 
+}
+  */  
+    
 class AdminUtilsTest extends PHPUnit_Framework_TestCase
 {
 	public function testAdminUtils()
 	{
         
-        $this->assertEquals('/index.php/admin/index/home/module/get/op/1', AdminUtils::set_admin_link('module', array('op' => 1)));
+        //Routes::$root_url.'index.php/admin/index/home/module/get/op/1', AdminUtils::set_admin_link('module', array('op' => 1))
+        
+        $this->assertEquals(Routes::$root_url.'index.php/admin/index/home/module/get/op/1', AdminUtils::set_admin_link('module', array('op' => 1)) );
         
     }
 
