cba73ef32812a66f55281396d51813879c9eefca
Antonio de la Rosa <webmaster@web-t-sys.com>
2016-02-18 17:02:40 +0100

    Little fix for hide the flash text

--- views/default/common/utils/flash.php
+++ views/default/common/utils/flash.php
@@ -1,14 +1,29 @@
 <?php
 
+use PhangoApp\PhaView\View;
+
 function flashView($text)
 {
+
+    View::$js[]='jquery.min.js';
 
 	?>
 	
 	<div class="flash">
 		<?php echo $text; ?>
 	</div>
+	<script language="javascript">
 	
+	function drop_flash()
+	{
+        
+        $('.flash').hide('slow');
+	
+	}
+	
+	setTimeout(drop_flash, 2000);
+	
+	</script>
 	<?php
 
 }
